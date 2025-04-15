9836ae352ee70c338abe627e97133a5621ba6094
Leandro N. Nogueira <LeandroNascimentoNogueira>
2025-04-14 22:29:51 -0300

    first commit

---
+++ src/main/java/com/coleiratech/Coleira/Tech/ColeiraTechApplication.java
@@ -0,0 +1,13 @@
+package com.coleiratech.Coleira.Tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColeiraTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColeiraTechApplication.class, args);
+	}
+
+}
