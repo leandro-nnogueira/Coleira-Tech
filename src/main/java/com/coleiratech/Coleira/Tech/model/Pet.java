f0b59aaeb096477aab1e67d4ad9a92a90bc8fb40
Leandro N. Nogueira <LeandroNascimentoNogueira@gmail.com>
2025-04-17 18:57:18 -0300

    feat(model): cria entidade Pet com relacionamento com Dono e Coleira

--- src/main/java/com/coleiratech/Coleira/Tech/model/Pet.java
+++ src/main/java/com/coleiratech/Coleira/Tech/model/Pet.java
@@ -1,4 +1,35 @@
 package com.coleiratech.Coleira.Tech.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+
+    private String tipo;
+
+    private String raca;
+
+    private String cor;
+
+    @ManyToOne
+    @JoinColumn(name = "dono_id", nullable = false)
+    private Dono dono;
+
+    @OneToOne(mappedBy = "pet", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Coleira coleira;
+
 }
