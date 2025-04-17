a7dfb7fbfca54c8eb5bc575cb820ff34034e3e38
Leandro N. Nogueira <LeandroNascimentoNogueira@gmail.com>
2025-04-17 18:57:31 -0300

    feat(model): cria entidade Coleira com relacionamento com Pet e Dono

--- src/main/java/com/coleiratech/Coleira/Tech/model/Coleira.java
+++ src/main/java/com/coleiratech/Coleira/Tech/model/Coleira.java
@@ -1,4 +1,29 @@
 package com.coleiratech.Coleira.Tech.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Coleira {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String identificador;
+
+    @OneToOne
+    @JoinColumn(name = "pet_id", nullable = false)
+    private Pet pet;
+
+    @ManyToOne
+    @JoinColumn(name = "dono_id", nullable = false)
+    private Dono dono;
 }
