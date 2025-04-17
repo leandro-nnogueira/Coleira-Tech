0694d9ba926ed3a92d0372a47769d48aa29f12fd
Leandro N. Nogueira <LeandroNascimentoNogueira@gmail.com>
2025-04-17 18:57:46 -0300

    feat(model): cria entidade Localizacao com vínculo a Pet e Coleira

--- src/main/java/com/coleiratech/Coleira/Tech/model/Localizacao.java
+++ src/main/java/com/coleiratech/Coleira/Tech/model/Localizacao.java
@@ -1,4 +1,37 @@
 package com.coleiratech.Coleira.Tech.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Localizacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private double latitude;
+
+    private double longitude;
+
+    private LocalDateTime dataHoraRecebimento;
+
+    private LocalDateTime dataHoraGPS;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id", nullable = false)
+    private Pet pet;
+
+    @ManyToOne
+    @JoinColumn(name = "coleira_id", nullable = false)
+    private Coleira coleira;
+
 }
