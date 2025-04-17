500439b2e0cce7dd9c5f3c6015e64c4dda6f08ad
Leandro N. Nogueira <LeandroNascimentoNogueira@gmail.com>
2025-04-17 18:56:57 -0300

    feat(model): cria entidade Dono com UUID e relacionamentos com Pet e Coleira

--- src/main/java/com/coleiratech/Coleira/Tech/model/Dono.java
+++ src/main/java/com/coleiratech/Coleira/Tech/model/Dono.java
@@ -1,4 +1,33 @@
 package com.coleiratech.Coleira.Tech.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Dono {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    @OneToMany(mappedBy = "dono", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Pet> pets;
+
+    @OneToMany(mappedBy = "dono", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Coleira> coleiras;
+
 }
